eb561aaf3e766e6f66a8d1af1c8abe6b269ca4a1
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-01 23:34:54 +0530

    Update problem178.c
    Refactored the program to track data usage with improved logic for counting exhausted days and overused data.

--- While loop/problem178.c
+++ While loop/problem178.c
@@ -1,18 +1,33 @@
+
 #include <stdio.h>
 
 int main(){
-    int total,n,usage;
-    scanf("%d %d", &total,&n);
-    int sum=0,count=0,count1=0;
-    while(n--){
+    int data,n,usage;
+    scanf("%d %d", &data,&n);
+    int num=0,sum=0,temp=0,count=0,rem=0;
+    int i=1;
+    while(i<=n){
         scanf("%d", &usage);
         sum=sum+usage;
-        if(sum<=total){
+        if(sum<=data){
             count++;
+        }else{
+            if(temp==0){
+                count++;
+                num=sum;
+                temp++;
+            }
         }
-        if(sum>=0){
-            count1++;
-        }
-    }printf("Exhausted Day: %d\n", count);
-    printf("Overcaused Day: %d", count1);
+        rem=num-data;
+        i++;
+    }if(sum>data){
+        printf("Exhausted Day: %d\n", count);
+    }else{
+        printf("Exhausted Day: Not Exhausted\n");
+    }
+    if(rem>0){
+    printf("Overused Data: %d\n", rem);
+    }else{
+        printf("Overused Data: 0\n");
+    }
 }
