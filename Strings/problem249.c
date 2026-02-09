e33e96bc65ae3bb5bcc14b64246266c24947081a
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-09 23:07:46 +0530

    Add files via upload

---
+++ Strings/problem249.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+   
+    for(int i=0;str[i]!='\0';i++){
+        str[i]=toupper(str[i]);
+    }
+    printf("%s", str);
+   
+}
