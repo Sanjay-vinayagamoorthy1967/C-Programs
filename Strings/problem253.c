d32c312e39c5c187e951977b95530909c9a06b79
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-02-10 20:31:21 +0530

    Add files via upload

---
+++ Strings/problem253.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char str[100];
+    fgets(str,sizeof(str),stdin);
+
+    char ch[100];
+    fgets(ch,sizeof(ch),stdin);
+    int index=0;
+    for(int i=0;str[i]!='\0' && str[i]!='\n';i++){
+        if(str[i]==ch[0]){
+            index=i;
+            break;
+        }
+    }
+      printf("%d", index);
+       
+    
+}
