5aaaaa73767a540d4c318c0969495ec9fd95ee92
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-03-25 17:48:00 +0530

    Add files via upload

---
+++ Strings/problem278.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s1[100], s2[100];
+    scanf("%s %s", s1, s2);
+
+    int freq[26] = {0};
+
+    // Count characters in s1
+    for(int i = 0; s1[i] != '\0'; i++) {
+        freq[s1[i] - 'a']++;
+    }
+
+    // Subtract characters in s2
+    for(int i = 0; s2[i] != '\0'; i++) {
+        freq[s2[i] - 'a']--;
+    }
+
+    // Count deletions
+    int deletions = 0;
+    for(int i = 0; i < 26; i++) {
+        if(freq[i] < 0)
+            deletions += -freq[i];
+        else
+            deletions += freq[i];
+    }
+
+    printf("%d", deletions);
+
+    return 0;
+}
