75f2bb2877135a15ae3731ba5222bcc1c20ec1c4
Sanjay Vinayagamoorthy <sanjay.v2382k05@gmail.com>
2026-08-12 20:12:42 +0530

    p

--- While loop/problem184.c
+++ While loop/problem184.c
@@ -3,7 +3,7 @@
 int main(){
     int total,n,usage;
     scanf("%d %d", &total,&n);   
-    int sum=0,count=0,count1=0; 
+    int sum=0,count=0,count1=0;  
     while(n--){     
         scanf("%d", &usage);       
         sum=sum+usage;            
